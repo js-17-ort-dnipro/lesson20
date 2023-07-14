02d1429ddf857a7155fad3a000e413e3099103ae
anatoliy.kigel@gmail.com <Anatoliy Kigel>
2023-07-14 10:03:41 +0300

    Upload Files & Source

---
+++ src/demo-template/assets/js/app.js
@@ -0,0 +1,8 @@
+
+    const countriesURL = 'https://restcountries.com/v3.1/all';
+
+    const usersURL = 'https://randomuser.me/api/?results=20';
+
+    const ratesURL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+
+    const storeAPI = 'https://fakestoreapi.com/products';
